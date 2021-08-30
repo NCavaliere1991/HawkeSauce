b3d9157b64572332d341ff177f661985e81ce58b
Nick Cavaliere <nickcavaliere1991@gmail.com>
2021-08-30 16:56:12 -0400

    fix functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,25 @@
 function sauceDropdown() {
     document.getElementById('sauceDropdown').classList.toggle("show")
+    if (document.getElementById('seasonDropdown').classList.contains('show') || document.getElementById('merchDropdown').classList.contains('show')) {
+        document.getElementById('seasonDropdown').classList.remove('show');
+        document.getElementById('merchDropdown').classList.remove('show');
+    }
 }
 
 function seasonDropdown() {
     document.getElementById('seasonDropdown').classList.toggle("show")
+    if (document.getElementById('sauceDropdown').classList.contains('show') || document.getElementById('merchDropdown').classList.contains('show')) {
+        document.getElementById('sauceDropdown').classList.remove('show');
+        document.getElementById('merchDropdown').classList.remove('show');
+    }
 }
 
 function merchDropdown() {
     document.getElementById('merchDropdown').classList.toggle("show")
+    if (document.getElementById('sauceDropdown').classList.contains('show') || document.getElementById('seasonDropdown').classList.contains('show')) {
+        document.getElementById('sauceDropdown').classList.remove('show');
+        document.getElementById('seasonDropdown').classList.remove('show');
+    }
 }
 
 window.onclick = function (event) {
