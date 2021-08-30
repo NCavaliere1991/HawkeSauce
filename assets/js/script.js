0c211a4063bbccaf9a1e8432ebeb03089fd43819
Nick Cavaliere <nickcavaliere1991@gmail.com>
2021-08-30 17:18:33 -0400

    dropdown fixed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,36 +1,55 @@
-function sauceDropdown() {
-    document.getElementById('sauceDropdown').classList.toggle("show")
-    if (document.getElementById('seasonDropdown').classList.contains('show') || document.getElementById('merchDropdown').classList.contains('show')) {
-        document.getElementById('seasonDropdown').classList.remove('show');
-        document.getElementById('merchDropdown').classList.remove('show');
-    }
-}
+// function sauceDropdown() {
+//     document.getElementById('sauceDropdown').classList.toggle("show")
+//     if (document.getElementById('seasonDropdown').classList.contains('show') || document.getElementById('merchDropdown').classList.contains('show')) {
+//         document.getElementById('seasonDropdown').classList.remove('show');
+//         document.getElementById('merchDropdown').classList.remove('show');
+//     }
+// }
 
-function seasonDropdown() {
-    document.getElementById('seasonDropdown').classList.toggle("show")
-    if (document.getElementById('sauceDropdown').classList.contains('show') || document.getElementById('merchDropdown').classList.contains('show')) {
-        document.getElementById('sauceDropdown').classList.remove('show');
-        document.getElementById('merchDropdown').classList.remove('show');
-    }
-}
+// function seasonDropdown() {
+//     document.getElementById('seasonDropdown').classList.toggle("show")
+//     if (document.getElementById('sauceDropdown').classList.contains('show') || document.getElementById('merchDropdown').classList.contains('show')) {
+//         document.getElementById('sauceDropdown').classList.remove('show');
+//         document.getElementById('merchDropdown').classList.remove('show');
+//     }
+// }
 
-function merchDropdown() {
-    document.getElementById('merchDropdown').classList.toggle("show")
-    if (document.getElementById('sauceDropdown').classList.contains('show') || document.getElementById('seasonDropdown').classList.contains('show')) {
-        document.getElementById('sauceDropdown').classList.remove('show');
-        document.getElementById('seasonDropdown').classList.remove('show');
-    }
+// function merchDropdown() {
+//     document.getElementById('merchDropdown').classList.toggle("show")
+//     if (document.getElementById('sauceDropdown').classList.contains('show') || document.getElementById('seasonDropdown').classList.contains('show')) {
+//         document.getElementById('sauceDropdown').classList.remove('show');
+//         document.getElementById('seasonDropdown').classList.remove('show');
+//     }
+// }
+
+// const dropdown = document.getElementsByClassName("dropbtn")
+
+// window.onclick = function (event) {
+//     if (!event.target.matches('.dropbtn')) {
+//         var dropdowns = document.getElementsByClassName("dropdown-content");
+//         var i;
+//         for (i = 0; i < dropdowns.length; i++) {
+//             var openDropdown = dropdowns[i]
+//             if (openDropdown.classList.contains('show')) {
+//                 openDropdown.classList.remove('show');
+//             }
+//         }
+//     }
+// }
+
+function dropdown() {
+    [].slice.call(document.getElementsByClassName("dropdown-content")).map(function (el) {
+        if (this.event.target.nextElementSibling !== el)
+            el.classList.remove("show");
+    });
+
+    this.event.target.nextElementSibling.classList.toggle("show");
 }
 
 window.onclick = function (event) {
     if (!event.target.matches('.dropbtn')) {
-        var dropdowns = document.getElementsByClassName("dropdown-content");
-        var i;
-        for (i = 0; i < dropdowns.length; i++) {
-            var openDropdown = dropdowns[i]
-            if (openDropdown.classList.contains('show')) {
-                openDropdown.classList.remove('show');
-            }
-        }
+        [].slice.call(document.getElementsByClassName("dropdown-content")).map(function (el) {
+            el.classList.remove("show");
+        });
     }
 }
